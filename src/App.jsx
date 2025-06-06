5ec44ce7b98a7447234e58c93c57f8ccc30ac5de
I-Am-Shimanto <shimantowebdeveloper@gmail.com>
2025-06-05 23:43:17 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import Layout from "./layout/Index"
+import Home from "./pages/Home"
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
