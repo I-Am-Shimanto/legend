3ea1cc5243cd6505964bb7aaf51ce78d25d27020
iamShimanto <shimantowebdeveloper@gmail.com>
2025-06-10 22:10:51 +0600

    client

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -8,11 +8,12 @@ const Clients = () => {
           <p className="text-sm font-normal font-inter text-black leading-5.25 px-4 py-1.5 bg-brand w-fit rounded-full mx-auto">
             Clients feedback
           </p>
-          <h2 className="text-2xl sm:text-3xl md:4xl lg:text-5xl font-semibold font-inter text-white sm:leading-13 max-w-160 text-center mx-auto pt-5.25 px-0">
+          <h2 className="text-2xl sm:text-3xl md:4xl lg:text-5xl font-semibold font-inter text-white sm:leading-13 max-w-160 text-center mx-auto pt-5.25 px-0 pb-8">
             What our <span className="text-brand">satisfied clients</span> said
             about us!
           </h2>
         </div>
+        <div className="grid"></div>
       </div>
     </section>
   );
